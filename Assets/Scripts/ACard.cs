e9c2c429adcd8ff1410c6ce706407c048c649b27
Alaeddine_Rekik <alaa.eddine.rekik@gmail.com>
2024-06-11 15:22:37 +0100

    Creating ACard Class:
    1/ The card has an usigned value.
    2/ The card can be flipped.

---
+++ Assets/Scripts/ACard.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ACard : MonoBehaviour
+{
+    [SerializeField] public uint Value { get; set; }
+    
+    public bool Isflipped { get; private set; }
+
+    public ACard()
+    {
+        Value = 0;
+        Isflipped = false;
+    }
+
+    public ACard(uint value) : this()
+    {
+        Value = value;
+    }
+
+    public void Flip()
+    {
+        Isflipped= !Isflipped;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
