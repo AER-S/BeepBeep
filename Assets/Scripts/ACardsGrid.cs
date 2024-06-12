46be2c423ee400396dcd939368f20d38e5dfdb82
Alaeddine_Rekik <alaa.eddine.rekik@gmail.com>
2024-06-12 23:49:21 +0100

    Scaling the Cards Depending on the layout

--- Assets/Scripts/ACardsGrid.cs
+++ Assets/Scripts/ACardsGrid.cs
@@ -12,11 +12,14 @@ public class ACardsGrid : MonoBehaviour
     [field:SerializeField]public int Columns { get; private set; }
 
     [SerializeField]private VisualProvider VisualProvider;
-    private ACardSlot[] _cardSlots;
     
+    
+    private ACardSlot[] _cardSlots;
+    private Vector3 _scale;
 
     void Start()
     {
+        _scale = GetScale();
         _cardSlots = new ACardSlot[Rows * Columns];
         for (int i = 0; i < _cardSlots.Length; i++)
         { 
@@ -26,10 +29,12 @@ public class ACardsGrid : MonoBehaviour
 
     private void SpawnACard(int position)
     {
-        var SpawnPosition = GetPosition(position);
-        _cardSlots[position] = Instantiate<ACardSlot>(CardSlotPrefab,SpawnPosition,Quaternion.identity);
-        _cardSlots[position].FillSlot(Instantiate<ACard>(CardPrefab, SpawnPosition, Quaternion.identity));
+        var spawnPosition = GetPosition(position);
+        _cardSlots[position] = Instantiate<ACardSlot>(CardSlotPrefab,spawnPosition,Quaternion.identity);
+        _cardSlots[position].transform.localScale = _scale;
+        _cardSlots[position].FillSlot(Instantiate<ACard>(CardPrefab, spawnPosition, Quaternion.identity));
         _cardSlots[position].Card.Value = position % VisualProvider.GetTextures().Length;
+        _cardSlots[position].Card.transform.localScale = _scale;
     }
 
     Vector3 GetPosition(int i)
@@ -41,5 +46,12 @@ public class ACardsGrid : MonoBehaviour
         float columnPosition = (Width /(float) Columns) * columnIndex;
         var spawnLocation = new Vector3(columnPosition, rowPosition, 0);
         return spawnLocation;
+    }
+
+    Vector3 GetScale()
+    {
+        float WidthScale = (Width-1) / ((float)Columns * 3);
+        float HeightScale = (Height-1) / ((float)Rows * 5);
+        return new Vector3(WidthScale, HeightScale, 1);
     }
 }
