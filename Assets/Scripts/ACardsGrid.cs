08ef22da288842a1cd0dda404ef93435aa20cfb1
Alaeddine_Rekik <alaa.eddine.rekik@gmail.com>
2024-06-12 20:12:37 +0100

    Placing the cards in a grid

--- Assets/Scripts/ACardsGrid.cs
+++ Assets/Scripts/ACardsGrid.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class ACardsGrid : MonoBehaviour
 {
+    [SerializeField] private int Width;
+    [SerializeField] private int Height;
     [SerializeField] private ACard CardPrefab;
     [SerializeField] private ACardSlot CardSlotPrefab;
-    [field:SerializeField]public uint Raws { get; private set; }
-    [field:SerializeField]public uint Columns { get; private set; }
+    [field:SerializeField]public int Rows { get; private set; }
+    [field:SerializeField]public int Columns { get; private set; }
 
     private ACardSlot[] _cardSlots;
     
 
     void Start()
     {
-        _cardSlots = new ACardSlot[Raws * Columns];
+        _cardSlots = new ACardSlot[Rows * Columns];
         for (int i = 0; i < _cardSlots.Length; i++)
         { 
-            _cardSlots[i] = Instantiate<ACardSlot>(CardSlotPrefab);
-            _cardSlots[i].FillSlot(Instantiate(CardPrefab));
+            SpawnACard(i);
         }
+    }
+
+    private void SpawnACard(int position)
+    {
+        var SpawnPosition = GetPosition(position);
+        _cardSlots[position] = Instantiate<ACardSlot>(CardSlotPrefab,SpawnPosition,Quaternion.identity);
+        _cardSlots[position].FillSlot(Instantiate(CardPrefab,SpawnPosition,Quaternion.identity));
+    }
+
+    Vector3 GetPosition(int i)
+    {
+        
+        int rowIndex = i / Columns;
+        int columnIndex =  i % Columns;
+        float rowPosition = (Height / (float)Rows) * rowIndex;
+        float columnPosition = (Width /(float) Columns) * columnIndex;
+        var spawnLocation = new Vector3(columnPosition, rowPosition, 0);
+        return spawnLocation;
     }
 }
