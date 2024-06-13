281defa95b999ae398b68d29ff393c2ae04b53eb
Alaeddine_Rekik <alaa.eddine.rekik@gmail.com>
2024-06-13 11:14:54 +0100

    Assingning random values to the cards:
    1/ Adding Variations variable.
    2/ Getting a random value for every variation.
    3/ Getting a distribution weight for every variation.
    4/ Assigning values to the cards randomly and based on weights

--- Assets/Scripts/ACardsGrid.cs
+++ Assets/Scripts/ACardsGrid.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
 
 public class ACardsGrid : MonoBehaviour
 {
@@ -12,28 +14,79 @@ public class ACardsGrid : MonoBehaviour
     [field:SerializeField]public int Columns { get; private set; }
 
     [SerializeField]private VisualProvider VisualProvider;
-    
+
+    [SerializeField] private int Variations;
     
     private ACardSlot[] _cardSlots;
     private Vector3 _scale;
+    
+    
 
     void Start()
     {
         _scale = GetScale();
-        _cardSlots = new ACardSlot[Rows * Columns];
+        var cardsCount = Rows * Columns;
+        _cardSlots = new ACardSlot[cardsCount];
+        var baseWeight = GetWeight();
+        var weightsDistribution = GetDistribution(baseWeight);
         for (int i = 0; i < _cardSlots.Length; i++)
-        { 
-            SpawnACard(i);
+        {
+            var cardValue = GetCardValueFrom(weightsDistribution);
+            SpawnACard(i,cardValue);
         }
     }
 
-    private void SpawnACard(int position)
+    private int GetWeight()
+    {
+        return (Rows * Columns) / Variations;
+    }
+
+    private Dictionary<int, int> GetDistribution(int baseWeight)
+    {
+        var upperWeightCount = (Rows * Columns) % Variations;
+        Debug.Log("UpperWeightCount= "+upperWeightCount);
+        Dictionary<int, int> Distributions = new Dictionary<int, int>();
+        for (int i = 0; i < Variations; i++)
+        {
+            int value = GetRandomValue(Distributions);
+            int weight = (i < upperWeightCount) ? (baseWeight + 1) : baseWeight;
+            Distributions.Add(value,weight);
+            Debug.Log("value= "+value+ " weight: "+ weight);
+        }
+
+        return Distributions;
+    }
+
+    private int GetRandomValue(Dictionary<int,int> Distributions)
+    {
+        int randomValue = 0;
+        do
+        {
+            randomValue = Random.Range(0, VisualProvider.GetTextures().Length);
+        } while (Distributions.ContainsKey(randomValue));
+
+        return randomValue;
+    }
+
+    private int GetCardValueFrom(Dictionary<int, int> weightsDistributions)
+    {
+
+        int value = weightsDistributions.ElementAt(Random.Range(0, weightsDistributions.Count)).Key;
+        int weight=0;
+        weightsDistributions.TryGetValue(value, out weight);
+        --weight;
+        if (weight <= 0) weightsDistributions.Remove(value);
+        else weightsDistributions[value] = weight;
+        return value;
+    }
+
+    private void SpawnACard(int position, int cardValue)
     {
         var spawnPosition = GetPosition(position);
         _cardSlots[position] = Instantiate<ACardSlot>(CardSlotPrefab,spawnPosition,Quaternion.identity);
         _cardSlots[position].transform.localScale = _scale;
         _cardSlots[position].FillSlot(Instantiate<ACard>(CardPrefab, spawnPosition, Quaternion.identity));
-        _cardSlots[position].Card.Value = position % VisualProvider.GetTextures().Length;
+        _cardSlots[position].Card.Value = cardValue;
         _cardSlots[position].Card.transform.localScale = _scale;
     }
 
