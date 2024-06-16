efd405a16b2b001eec8c0d64530a3fa265889887
Alaeddine_Rekik <alaa.eddine.rekik@gmail.com>
2024-06-16 16:25:39 +0100

    Cleaning Sound Manager

--- Assets/Scripts/ASoundsManager.cs
+++ Assets/Scripts/ASoundsManager.cs
@@ -3,18 +3,19 @@ using UnityEngine;
 
 public class ASoundsManager : Singleton<ASoundsManager>
 {
+    #region SerializeField
+
     [SerializeField] private AudioClip FlippingSound;
-
     [SerializeField] private AudioClip MatchingSuccessSound;
-
     [SerializeField] private AudioClip MatchingFailedSound;
-
     [SerializeField] private AudioClip GameOverSound;
-
     [SerializeField] private AudioSource CardAudioSource;
-
     [SerializeField] private AudioSource MatchingAudioSource;
-    
+
+    #endregion
+
+    #region Unity Events
+
     private void OnEnable()
     {
         AGameManager.Instance.MatchingSuccess += PlaySuccessSound;
@@ -29,6 +30,8 @@ public class ASoundsManager : Singleton<ASoundsManager>
         AGameManager.Instance.MatchingFailed -= PlayFailSound;
         AGameManager.Instance.GameOver -= PlayGameOverSound;
     }
+
+    #endregion
     private void PlayGameOverSound(bool value)
     {
         MatchingAudioSource.PlayOneShot(GameOverSound);
