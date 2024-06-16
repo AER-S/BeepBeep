3bfec997c47af689b81ce873b07c7596f7cc2de4
Alaeddine_Rekik <alaa.eddine.rekik@gmail.com>
2024-06-16 16:27:51 +0100

    Cleaning End Game Panel Controller

--- Assets/Scripts/UI/AEndGamePanelController.cs
+++ Assets/Scripts/UI/AEndGamePanelController.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 
 public class AEndGamePanelController : MonoBehaviour
 {
+    #region SerializeField
+
     [SerializeField] private GameObject GameOverPanel;
     [SerializeField] private TMP_Text Title;
     [SerializeField] private Button ContinueButton;
@@ -13,6 +15,9 @@ public class AEndGamePanelController : MonoBehaviour
     [SerializeField] private Button HomeButton;
     [SerializeField] private Button QuitButton;
 
+    #endregion
+
+    #region Unity Events
 
     private void OnEnable()
     {
@@ -32,6 +37,8 @@ public class AEndGamePanelController : MonoBehaviour
         ContinueButton.onClick.RemoveListener(Continue);
         HomeButton.onClick.AddListener(GoHome);
     }
+
+    #endregion
 
     
     private void ProcessEndGame(bool gameWon)
@@ -56,9 +63,7 @@ public class AEndGamePanelController : MonoBehaviour
     
     private void GoHome()
     {
-        if (AGameManager.Instance.IsWin)
-            ASavingManager.Instance.GameData.GameMode = AMainMenuController.AGameMode.WinStrike;
-        else ASavingManager.Instance.GameData.GameMode = AMainMenuController.AGameMode.NewGame;
+        ASavingManager.Instance.GameData.GameMode = AGameManager.Instance.IsWin ? AMainMenuController.AGameMode.WinStrike : AMainMenuController.AGameMode.NewGame;
         AHUDController.Instance.GoHome();
     }
 
@@ -74,16 +79,5 @@ public class AEndGamePanelController : MonoBehaviour
         ASavingManager.Instance.GameData.GameMode = AMainMenuController.AGameMode.WinStrike;
         SceneManager.LoadScene(1);
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        GameOverPanel.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
